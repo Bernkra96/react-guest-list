10f1d1b58f1ea19810551330de6965b84b760008
Mona Lisa Octocat <110023253+Bernkra96@users.noreply.github.com>
2023-09-24 00:09:41 +0200

    Uptatte

--- src/App.1.js
+++ src/App.1.js
@@ -5,15 +5,15 @@ export default function App() {
   const [inputLastName, setInputLastName] = React.useState('');
   // const [guestList, setGustList] = useState([]);
   // const [isChecked, setIsChecked] = useState(false);
-  // function addGust() {
-  // return AddGust(inputFirstName, inputLastName);
-  // }
+
   // const deleteById = (id) => {
   // return DeleteGust(id);
   // };
+
   // function setAddening  (id)  {
   // return UpdateGust(id);
   // };
+
   const baseUrl = 'http://localhost:4000';
 
   // async function BaseURl() {
@@ -21,25 +21,27 @@ export default function App() {
   // .then((res) => res.json())
   // .then((data) => console.log(data));
   // }
-  // async function GuestList() {
-  // await fetch(`${baseUrl}/guests`);
-  // }
+
+  async function GuestList() {
+    await fetch(`${baseUrl}/guests`).then((res) => res.json());
+  }
+
   // async function GetSingleGust(id) {
   /// await fetch(`${baseUrl}/guests/${id}`)
   //  .then((res) => res.json())
   //  .then((data) => console.log(data));
   // }
-  // async function AddGust(firstName, lasstName) {
-  // await fetch(`${baseUrl}/guests`, {
-  // method: 'POST',
-  // headers: {
-  // 'Content-Type': 'application/json',
-  // },
-  //  body: JSON.stringify({ firstName: firstName, lastName: lasstName }),
-  // })
-  //  .then((res) => res.json())
-  //  .then((data) => console.log(data));
-  // }
+
+  async function AddGust(firstName, lasstName) {
+    await fetch(`${baseUrl}/guests`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ firstName: firstName, lastName: lasstName }),
+    }).then((res) => res.json());
+  }
+
   //  async function UpdateGust(id) {
   //    await fetch(`${baseUrl}/guests/${id}`, {
   //      method: 'PUT',
@@ -51,11 +53,13 @@ export default function App() {
   //     .then((res) => res.json())
   //     .then((data) => console.log(data));
   // }
+
   // async function DeleteGust(id) {
   // await fetch(`${baseUrl}/guests/${id}`, { method: 'DELETE' })
   //  .then((res) => res.json())
   //  .then((data) => console.log(data));
   // }
+
   return (
     <body>
       <header>
@@ -64,7 +68,33 @@ export default function App() {
       <main>
         <h2>Add Gust</h2>
 
-        <ol></ol>
+        <ol>{(GuestList.map = (gust) => <li> {gust.id} </li>)}</ol>
+
+        <div>
+          <form>
+            <label>
+              First name
+              <input
+                name="name"
+                value={inputFirstName}
+                onChange={(e) => setInputFirstName(e.target.value)}
+              />
+            </label>
+            <label>
+              Last name
+              <input
+                name="name"
+                value={inputLastName}
+                onChange={(e) => setInputLastName(e.target.value)}
+              />
+            </label>
+            <input
+              type="submit"
+              value="Add Gust"
+              onClick={AddGust(inputFirstName, inputLastName)}
+            />
+          </form>
+        </div>
       </main>
     </body>
   );
